b47aeb4776ec553e9748caab5f231d8bc1cf72f6
jkuya <kuyajesee@proton.me>
2025-05-09 15:50:07 +0300

    Fix saveAccountToFile function to use pointers for User and Record structures

--- src/system.c
+++ src/system.c
@@ -18,21 +18,22 @@ int getAccountFromFile(FILE *ptr, char name[50], struct Record *r)
                   r->accountType) != EOF;
 }
 
-void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
+void saveAccountToFile(FILE *ptr, struct User *u, struct Record *r)
 {
     fprintf(ptr, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n\n",
-            &r->id,
-	    &u->id
-	    &u->name,
-            r.accountNbr,
-            r.deposit.month,
-            r.deposit.day,
-            r.deposit.year,
-            r.country,
-            r.phone,
-            r.amount,
-            r.accountType);
+            r->id,
+            u->id,
+            u->name,
+            r->accountNbr,
+            r->deposit.month,
+            r->deposit.day,
+            r->deposit.year,
+            r->country,
+            r->phone,
+            r->amount,
+            r->accountType);
 }
+
 
 void stayOrReturn(int notGood, void f(struct User u), struct User u)
 {
