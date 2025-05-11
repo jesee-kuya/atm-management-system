0d3d88140caaaf5aa1302977a6c1dea7f4aa657a
jkuya <kuyajesee@proton.me>
2025-05-12 00:25:58 +0300

    Enhance login and registration menus with improved user prompts and password handling

--- src/auth.c
+++ src/auth.c
@@ -1,30 +1,28 @@
 #include <termios.h>
+#include <unistd.h>
 #include "header.h"
 
 char *USERS = "./data/users.txt";
 
 void loginMenu(char a[50], char pass[50]) {
-    struct termios oflags, nflags;
-
+    struct termios oldt, newt;
     system("clear");
-    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    printf("\n\n\t\t\t\t╔════════════════════════════╗");
+    printf("\n\t\t\t\t║       User Login           ║");
+    printf("\n\t\t\t\t╚════════════════════════════╝\n");
+    printf("\n\t\t\t\tEnter username: ");
     scanf("%49s", a);
 
-    // Disabling echo
-    tcgetattr(fileno(stdin), &oflags);
-    nflags = oflags;
-    nflags.c_lflag &= ~ECHO;
-    nflags.c_lflag |= ECHONL;
+    // Disable echo for password
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0) {
-        perror("tcsetattr");
-        exit(1);
-    }
-    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    printf("\t\t\t\tEnter password: ");
     scanf("%49s", pass);
-
-    // Restore terminal
-    tcsetattr(fileno(stdin), TCSANOW, &oflags);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    printf("\n");
 }
 
 int getUser(struct User *u) {
@@ -45,34 +43,41 @@ int getUser(struct User *u) {
 
 void registerMenu(char a[50], char pass[50]) {
     struct User newUser = {0};
+    int maxId = 0;
     FILE *fp = fopen(USERS, "r");
-    
+
     // Find max ID
-    while (fp && fscanf(fp, "%d %*s %*s", &newUser.id) != EOF) {
-        if (newUser.id > newUser.id) newUser.id = newUser.id;
+    if (fp) {
+        int currentId;
+        while (fscanf(fp, "%d %*s %*s", &currentId) != EOF) {
+            if (currentId > maxId) maxId = currentId;
+        }
+        fclose(fp);
     }
-    newUser.id++;
-    if (fp) fclose(fp);
+    newUser.id = maxId + 1;
 
     system("clear");
-    printf("\n\n\t\t\t\tEnter username: ");
+    printf("\n\n\t\t\t\t╔════════════════════════════╗");
+    printf("\n\t\t\t\t║       Registration        ║");
+    printf("\n\t\t\t\t╚════════════════════════════╝\n");
+    printf("\n\t\t\t\tEnter username: ");
     scanf("%49s", newUser.name);
-    printf("\n\t\t\t\tEnter password: ");
+    printf("\t\t\t\tEnter password: ");
     scanf("%49s", newUser.password);
 
     fp = fopen(USERS, "a");
     if (!fp) {
         perror("Failed to open users file");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     fprintf(fp, "%d %s %s\n", newUser.id, newUser.name, newUser.password);
     fclose(fp);
 
     strcpy(a, newUser.name);
     strcpy(pass, newUser.password);
-    printf("\n\t\t\t\tRegistration successful!\n");
+    printf("\n\t\t\t\t✅ Registration successful!\n");
+    sleep(1);
 }
-
 const char *getPassword(struct User u)
 {
     FILE *fp;
