52c6e7902909d3cc14b63d491a76889a18902ee2
jkuya <kuyajesee@proton.me>
2025-05-14 00:30:54 +0300

    Add ctype.h for character handling and clean up header includes

--- src/header.h
+++ src/header.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // Added for sleep()
+#include <unistd.h> 
+#include <ctype.h>
 
 struct Date {
     int month, day, year;
