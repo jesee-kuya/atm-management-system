b133465ed7106b4574f800688d1a14a66c47d0e8
jkuya <kuyajesee@proton.me>
2025-05-25 01:01:23 +0300

    Refactor isValidPassword function: change parameter type from const char* to char*

--- src/header.h
+++ src/header.h
@@ -58,5 +58,5 @@ int isValidAccountType(const char *type);
 int isValidName(const char *name);
 int isValidCountry(const char *country);
 void trimNewline(char *str);
-int isValidPassword(const char *password);
+int isValidPassword(char *password);
 void removeWhitespace(char *str);
